ee9d0f6937c1e1c66ec9c498bfd32fe042627a2a
GotoRen <r0719en@pluslab.org>
2021-04-22 17:30:18 +0900

    🔨 マウントボリュームの権限周りを調整

--- db/script/container-init.sh
+++ db/script/container-init.sh
@@ -8,12 +8,9 @@ if [ ! -e '/check' ]; then
     touch /check
 
     echo "Runninng Setup."
-    groupadd -g ${GID} ${UNAME}
-    useradd -u ${UID} -g ${UNAME} -m ${UNAME}
     chown ${UNAME}:${UNAME} -R /var/lib/mysql
-    echo "Complete Owner Setup!!"
 else
     echo "Already Setup." 
-    chown ${UNAME}:${UNAME} -R /var/lib/mysql
-    echo "Complete Owner Setup!!"
 fi
+
+/bin/bash
